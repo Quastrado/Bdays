6d0a029547d250d62fd343c5a7e0f1df095c244d
Quastrado <you@example.com>
2020-04-01 21:28:24 +0300

    route add separating

--- Bdays/routes.py
+++ Bdays/routes.py
@@ -2,8 +2,12 @@ from flask import current_app as app
 from Bdays.views.add import Add
 
 
-@app.route('/add', methods=['GET','POST'])
-def add():
+@app.route('/add', methods=['GET'])
+def add_get():
+    add = Add()
+    return add.render()
+
+@app.route('/add', methods=['POST'])
+def add_post():
     add = Add()
     return add.add_processing()
-
--- Bdays/views/add.py
+++ Bdays/views/add.py
@@ -1,20 +1,31 @@
-from flask import render_template, request
+from flask import flash, render_template
 from Bdays.forms import BirthdaysForm
 from Bdays.models import db, Birthdays
 
 class Add():
 
+    def render(self):
+        form = BirthdaysForm()
+        return render_template('add.html', form=form)
+    
     def add_processing(self):
         form = BirthdaysForm()
-                
-        if request.method == 'POST': 
-            if form.add.data:
-                if form.validate_on_submit():
-                    nickname = form.nickname.data
-                    date = form.date.data
-                    db_set = Birthdays(nickname = form.nickname.data, date = form.date.data)
-                    db.session.add(db_set)
-                    db.session.flush()
-                    db.session.commit()
 
-        return render_template('add.html', form=form)
+        if not form.validate_on_submit():
+            self.render()
+        
+        exists = db.session.query(Birthdays.nickname).filter(
+                    Birthdays.nickname == form.nickname.data
+                    ).scalar()
+        if exists is not None:
+            #flash('Specified nickname already exists')
+            form.nickname.data = ''
+            self.render()
+            
+        nickname = form.nickname.data
+        date = form.date.data
+        db_set = Birthdays(nickname = form.nickname.data, date = form.date.data)
+        db.session.add(db_set)
+        db.session.flush()
+        db.session.commit()
+        return self.render() # optionally, this may return flash later. May be
