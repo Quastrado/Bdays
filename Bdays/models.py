a12754dac863f3a0cfd11c57f2084d3c3a82a46e
Quastrado <you@example.com>
2020-04-02 02:52:06 +0300

    models recreating 0.1

--- Bdays/models.py
+++ Bdays/models.py
@@ -7,11 +7,11 @@ db = SQLAlchemy()
 class StudioMembers(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nickname = db.Column(db.String(50), unique=True)
-    date = db.Column(db.DateTime)
-
+    date = db.Column(db.DateTime) 
+    donations = db.relationship('Donations', backref='birthday')
 
 class Donations(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     amount = db.Column(db.Integer)
-    donor = db.Column(db.String(50), unique=True)
-    birthday_boy = db.Column(db.String(50), unique=True)
+    donor = db.Column(db.String(50))
+    birthday_boy = db.Column(db.Integer, db.ForeignKey('studio_members.id'))
