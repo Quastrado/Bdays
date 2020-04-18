ed4cb7c050c99bd838e2637c69aefc0d279c3e53
Quastrado <you@example.com>
2020-04-19 02:07:04 +0300

    fourth argument for donation controller

--- Bdays/view_models/donation.py
+++ Bdays/view_models/donation.py
@@ -1,7 +1,8 @@
 
 
 class Donation():
-    def __init__(self, donator, amount, description):
-        self.donator = donator
+    def __init__(self, donation_target, donation_source, amount, description):
+        self.donation_target = donation_target
+        self.donation_source = donation_source
         self.amount = amount
         self.description = description
