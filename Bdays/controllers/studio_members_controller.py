c9953d54b70d1933f6a613f3592193cb14c24d04
Quastrado <you@example.com>
2020-05-01 05:08:25 +0300

    route for new studio member creation (draft) #1

--- Bdays/controllers/studio_members_controller.py
+++ Bdays/controllers/studio_members_controller.py
@@ -1,8 +1,12 @@
+import uuid
+
 from flask import current_app as app
 from flask import jsonify, render_template, request
 from flask import Blueprint
 
+from Bdays.view_models.studio_member import StudioMember as ViewStudioMember
 from Bdays.DAL.studio_member_repository import StudioMemberRepository
+from Bdays.controllers.helper import convert_input_to
 
 
 blueprint = Blueprint('studio_member_controller', __name__, static_folder='static')
@@ -21,5 +25,18 @@ def get_by_id(id):
                             )
 
 
+@blueprint.route('/create_studio_member', methods=['POST'])
+@convert_input_to(ViewStudioMember)
+def create_studio_member(view_studio_member):
+    studio_member_repository = StudioMemberRepository()
+    new_studio_member_id = uuid.uuid4()
+    studio_member_repository.create(
+        new_studio_member_id,
+        view_studio_member.nickname,
+        view_studio_member.birthday,
+        view_studio_member.role
+    )
+    print(f'User with id - {new_studio_member_id}')
+    return "1" 
 
     
