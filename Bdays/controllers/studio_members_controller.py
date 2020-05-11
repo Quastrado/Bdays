807a7caebf67e51326bc53e2716b051f1afafcdb
Quastrado <you@example.com>
2020-05-11 08:34:22 +0300

    #1 routes, new view models import, endpoints

--- Bdays/controllers/studio_members_controller.py
+++ Bdays/controllers/studio_members_controller.py
@@ -2,7 +2,7 @@ from flask import current_app as app
 from flask import jsonify, render_template, request
 from flask import Blueprint
 
-from Bdays.view_models.studio_member import StudioMember as ViewStudioMember
+from Bdays.view_models.studio_member import StudioMember as ViewStudioMember, StudioMemberPassword as ViewStudioMemberPassword
 from Bdays.DAL.studio_member_repository import StudioMemberRepository
 from Bdays.controllers.helper import convert_input_to
 
@@ -23,7 +23,7 @@ def get_by_id(id):
                             )
 
 
-@blueprint.route('/', methods=['POST'])
+@blueprint.route('/', methods=['POST'], endpoint='create_studio_member')
 @convert_input_to(ViewStudioMember)
 def create_studio_member(view_studio_member):
     studio_member_repository = StudioMemberRepository()
@@ -38,5 +38,22 @@ def create_studio_member(view_studio_member):
         return 'Internal server error ', 500
     
     return 'Hello', 201
+
+
+@blueprint.route('/set_password/<id>', methods=['GET'])
+def render_set_password(id):
+    return render_template('set_password.html', id=id)
+
+
+@blueprint.route('/set_password', methods=['POST'], endpoint='set_password')
+@convert_input_to(ViewStudioMemberPassword)
+def set_password(view_studio_member_password):
+    studio_member_repository = StudioMemberRepository()
+    print(view_studio_member_password.id, view_studio_member_password.password)
+    studio_member_repository.set_password(
+        view_studio_member_password.id,
+        view_studio_member_password.password
+    )
+    return '', 201
 
     
