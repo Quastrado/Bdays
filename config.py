0bce34a1566cb4e85dae5beb27813828a2b11296
Quastrado <you@example.com>
2020-05-17 05:44:23 +0300

    #2 launch fixes first commit

---
+++ config.py
@@ -0,0 +1,24 @@
+import ast
+import os
+from datetime import timedelta
+
+class BaseConfig:
+	SECRET_KEY = os.getenv('SECRET_KEY')
+	DEBUG = True
+	REMEMBER_COOKIE_DURATION = timedelta(days=3)
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	POSTGRES = eval(os.getenv('POSTGRES'))
+	SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:%(password)s@%(host)s/%(database)s' % POSTGRES
+	
+	# GMAIL = 'EthexEnterprise@gmail.com'dashboard
+	# GMAIL_PASSWORD = 'sfabdays' 
+	# email server
+	MAIL_SERVER = os.getenv('MAIL_SERVER')
+	MAIL_PORT = os.getenv('MAIL_PORT')
+	MAIL_USE_TLS = False
+	MAIL_USE_SSL = True
+	MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+	MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+
+class TestConfig:
+    SECRET_KEY = os.getenv('SECRET_KEY')
