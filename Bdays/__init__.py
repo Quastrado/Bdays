3d48de721b143b8f85f3f20004a59220937d1266
Quastrado <you@example.com>
2020-05-24 02:08:36 +0300

    #2 create database; manager

--- Bdays/__init__.py
+++ Bdays/__init__.py
@@ -2,12 +2,18 @@ from flask import Flask
 from Bdays.DAL.models.db import db
 from flask import Blueprint
 from flask_login import LoginManager, current_user, login_required
-from flask_migrate import Migrate
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
 from Bdays.DAL.models.studio_member import StudioMember
 from Bdays.controllers.login_controller import blueprint as login_blueprint
 from Bdays.controllers.studio_members_controller import blueprint as studio_member_blueprint
 from Bdays.controllers.index_controller import blueprint as index_blueprint
 from Bdays.controllers.donations_controller import blueprint as donation_blueprint
+
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+
+from config import BaseConfig
 
 
 def create_app(test_config = False):
@@ -20,12 +26,20 @@ def create_app(test_config = False):
         app.config.from_object('config.TestConfig')
     else:
         app.config.from_object('config.BaseConfig')
-                
+        
     db.init_app(app)
     migrate = Migrate(app, db)
+    manager = Manager(app)
+    manager.add_command('db', MigrateCommand)
+    manager.add_command('runserver', Server(host=app.config['HOST'], port=app.config['PORT']))
     login_manager = LoginManager()
     login_manager.init_app(app)
     login_manager.login_view = '/'
+    
+
+    engine = create_engine(BaseConfig.SQLALCHEMY_DATABASE_URI)
+    if not database_exists(engine.url):
+        create_database(engine.url)
     
 
     @login_manager.user_loader
@@ -35,4 +49,4 @@ def create_app(test_config = False):
     with app.app_context():
         from Bdays.controllers import studio_members_controller, index_controller, login_controller
     
-    return app
+    return manager
