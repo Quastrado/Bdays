68b3caf10bd6fb82432cd825360381414820e18c
Quastrado <you@example.com>
2020-06-06 19:27:08 +0300

    #4 message body update

--- Bdays/services/email_service.py
+++ Bdays/services/email_service.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_mail import Mail, Message
 
 from flask import current_app as app
@@ -10,7 +11,8 @@ class EmailSender():
         mail = Mail(app)
         sender = BaseConfig.MAIL_USERNAME
         recipient = email
-        msg = Message('Whellcome', sender=sender, recipients=[recipient])
-        msg.body = 'http://127.0.0.1:5000/studio_member/set_password/{}'.format(studio_memeber_id)
+        id = studio_memeber_id
+        msg = Message('Wellcome', sender=sender, recipients=[recipient])
+        msg.body = f'http://{request.host}/studio_member/set_password/{id}'
         with app.app_context():
             mail.send(msg)
