75998808a8ad5f7c15d1f3d9d26033e82683c9a0
Quastrado <you@example.com>
2020-06-08 21:23:42 +0300

    #4 .gitignore changes

--- start.sh
+++ start.sh
@@ -3,17 +3,17 @@
 export FLASK_APP="Bdays"
 export FLASK_ENV="development"
 export FLASK_DEBUG="1"
-export HOST="your host"
-export PORT="your port"
+export HOST="0.0.0.0"
+export PORT="5000"
 
 export SECRET_KEY="your secret key"
-export DATABASE_URI="postgresql://postgres:postgres@127.0.0.1/your_db_name"
-export MAIL_SERVER='your smtp server'
+export DATABASE_URI="postgresql://bdays:sfa@127.0.0.1/bdays"
+export MAIL_SERVER='smtp.yandex.com'
 export MAIL_PORT=465
 export MAIL_USE_TLS=False
 export MAIL_USE_SSL=True
-export MAIL_USERNAME='your mail username'
-export MAIL_PASSWORD='your mail password'
+export MAIL_USERNAME='EthexEnterprise@yandex.ru'
+export MAIL_PASSWORD='sfabdays'
 python main.py db upgrade
 pytest
 python main.py runserver
