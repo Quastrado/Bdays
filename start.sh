c23d31301b5591db3a01fda220e84e09ed6d37fc
Quastrado <you@example.com>
2020-06-08 04:52:56 +0300

    #4 start.sh (again)

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+export FLASK_APP="Bdays"
+export FLASK_ENV="development"
+export FLASK_DEBUG="1"
+export HOST="your host"
+export PORT="your port"
+
+export SECRET_KEY="your secret key"
+export DATABASE_URI="postgresql://postgres:postgres@127.0.0.1/your_db_name"
+export MAIL_SERVER='your smtp server'
+export MAIL_PORT=465
+export MAIL_USE_TLS=False
+export MAIL_USE_SSL=True
+export MAIL_USERNAME='your mail username'
+export MAIL_PASSWORD='your mail password'
+python main.py db upgrade
+pytest
+python main.py runserver
