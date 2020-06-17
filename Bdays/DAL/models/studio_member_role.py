f3d4cab1d64774d54c55b8a5045c7266a9af8065
Quastrado <you@example.com>
2020-06-17 23:33:55 +0300

    #3 last role

--- Bdays/DAL/models/studio_member_role.py
+++ Bdays/DAL/models/studio_member_role.py
@@ -10,4 +10,3 @@ class StudioMemberRole(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     studio_member_id = db.Column(UUID(as_uuid=True), db.ForeignKey('studio_members.id', ondelete='CASCADE'), nullable=False)
     role_id = db.Column(UUID(as_uuid=True), db.ForeignKey('roles.id', ondelete='CASCADE'), nullable=False)
-
