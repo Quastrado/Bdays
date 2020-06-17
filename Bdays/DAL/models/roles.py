bf1c097a801069e63e416d705605acb50b90c7e8
Quastrado <you@example.com>
2020-06-17 23:33:10 +0300

    #3 default id in role model

--- Bdays/DAL/models/roles.py
+++ Bdays/DAL/models/roles.py
@@ -1,3 +1,5 @@
+import uuid
+
 from sqlalchemy.dialects.postgresql import UUID
 from flask_sqlalchemy import SQLAlchemy
 from Bdays.DAL.models.db import db
@@ -5,5 +7,5 @@ from Bdays.DAL.models.db import db
 
 class Role(db.Model):
     __tablename__ = 'roles'
-    id = db.Column(UUID(as_uuid=True), primary_key=True, nullable=False)
+    id = db.Column(UUID(as_uuid=True), primary_key=True, nullable=False, default=uuid.uuid4)
     role = db.Column(db.String(50), unique=True)
