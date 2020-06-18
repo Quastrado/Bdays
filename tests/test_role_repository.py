853a83a7818cab02a7e2ad7ed433cc36a3cbd075
Quastrado <you@example.com>
2020-06-18 15:32:27 +0300

    #3 read method tests

--- tests/test_role_repository.py
+++ tests/test_role_repository.py
@@ -1,6 +1,8 @@
 import pytest
 import uuid
 
+from sqlalchemy.exc import DataError, ProgrammingError
+
 from tests.test_base import app
 from Bdays.DAL.models.db import db
 from Bdays.DAL.models.roles import Role
@@ -28,3 +30,37 @@ def test_create_without_id(client):
 def test_create_missing_role(client):
     with pytest.raises(TypeError):
         repository.create()
+
+
+def test_create_incorrect_id(client):
+    id = 123
+    role = 'Tester'
+    with pytest.raises(DataError):
+        repository.create(id, role)
+
+
+def test_create_incorrect_role(client):
+    role = 123
+    role_id = repository.create(role)
+    assert repository.read(role_id)
+    repository.delete(role_id)
+
+
+"""read method tests"""
+def test_read_successfull(client):
+    role = 'Tester'
+    role_id = repository.create(role)
+    assert repository.read(role_id)
+    repository.delete(role_id)
+
+
+def test_read_missing_id(client):
+    with pytest.raises(TypeError):
+        repository.read()
+
+
+def test_read_incorrect_id(client):
+    id = 123
+    with pytest.raises(ProgrammingError):
+        repository.read(id)
+        
