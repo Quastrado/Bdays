0bd61e8177df9c9553af546f07d5991e302e47d1
Quastrado <you@example.com>
2020-06-24 21:21:10 +0300

    #3 gitignore and config file updates

--- config.py
+++ config.py
@@ -4,22 +4,22 @@ from datetime import timedelta
 
 
 class BaseConfig:
-	SECRET_KEY = os.getenv('SECRET_KEY')
+	SECRET_KEY = "your secret key"
 	DEBUG = True
-	HOST = os.getenv('HOST')
-	PORT = os.getenv('PORT')
+	HOST = "your host"
+	PORT = "your port"
 	REMEMBER_COOKIE_DURATION = timedelta(days=3)
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
-	MAIL_SERVER = os.getenv('MAIL_SERVER')
-	MAIL_PORT = os.getenv('MAIL_PORT')
+	SQLALCHEMY_DATABASE_URI = "your database uri"
+	MAIL_SERVER = "your mail server"
+	MAIL_PORT = "your mail port"
 	MAIL_USE_TLS = False
 	MAIL_USE_SSL = True
-	MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-	MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+	MAIL_USERNAME = "your mail username"
+	MAIL_PASSWORD = "your mail password"
 
 class TestConfig:
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = "your secret key"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@127.0.0.1/bdays"
+    SQLALCHEMY_DATABASE_URI = "your database uri"
 
