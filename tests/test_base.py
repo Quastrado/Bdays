e99b906357dde84e7bf18b1040081d358eccc639
Quastrado <you@example.com>
2020-06-24 21:19:04 +0300

    #3 test of dashboard start

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,9 +1,17 @@
 import pytest
+import json
+
+from flask import url_for
 
 from Bdays import create_app
 
 
 @pytest.fixture
 def app():
-    app=create_app(test_config=True)
+    app = create_app(test_config=True)
     return app
+
+
+def test_example(client):
+    response = client.get("/")
+    assert response.status_code == 200
