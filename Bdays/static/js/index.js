044fcab681349a0b312d476bc792904438a47282
Quastrado <you@example.com>
2020-07-08 20:52:17 +0300

    #5 filedialog button

--- Bdays/static/js/index.js
+++ Bdays/static/js/index.js
@@ -1,25 +1,33 @@
 
 
 $(document).ready(function () {
-    $('#personalArea').hide();
-    $('#errorAlert').hide();
-    var $body = $('body');
-  
-    $body.on('click', 'div.master_list div.list-group button', function () {
-      var $button = $(this),
-        article_option = $button.attr('data-option');
-        console.log(article_option)
-      $.ajax({
-        type: 'GET',
-        url: "/studio_member/" + article_option,
-        success: function(data){
-          $('#content').html(data);
-        },
-      });
-    });
-   });
+  // $('#imagePicker').click(function () {
+  //   console.log($)
+  //   $.FileDialog({ multiple: false }).on('files.bs.filedialog', function (ev) {
+  //     var file = ev.file;
+  //     console.log(file);
+  //   });
+  // });
 
-$('#newStudioMemberSubmit').click(function() {
+  $('#personalArea').hide();
+  $('#errorAlert').hide();
+  var $body = $('body');
+
+  $body.on('click', 'div.master_list div.list-group button', function () {
+    var $button = $(this),
+      article_option = $button.attr('data-option');
+    console.log(article_option)
+    $.ajax({
+      type: 'GET',
+      url: "/studio_member/" + article_option,
+      success: function (data) {
+        $('#content').html(data);
+      },
+    });
+  });
+});
+
+$('#newStudioMemberSubmit').click(function () {
   var email = $('#emailInput').val();
   var nickname = $('#nicknameInput').val();
   var birthday = $('#birthdayInput').val();
@@ -29,34 +37,37 @@ $('#newStudioMemberSubmit').click(function() {
     nickname: nickname,
     birthday: birthday,
     role: role
-    }
+  }
   console.log(data);
-    $.ajax({
+  $.ajax({
     url: "/studio_member/",
     type: 'POST',
     contentType: "application/json; charset=utf-8",
     data: JSON.stringify(data),
-    error: function(response) {
+    error: function (response) {
       console.log(response)
       $('#alertText').text(response.status + ' ' + response.responseText);
       $('#errorAlert').fadeIn('slow');
-      
+
     }
-    
+
   });
 });
 
 
-  $('#editProfile').click(function() {
-    $('#dashboard').hide();
-    $('#personalArea').show();
-  });
+$('#editProfile').click(function () {
+  $('#dashboard').hide();
+  $('#personalArea').show();
+});
 
 
-  $('#personalAreaClose').click(function() {
-    $('#personalArea').hide();
-    $('#dashboard').show();
-  });
+$('#personalAreaClose').click(function () {
+  $('#personalArea').hide();
+  $('#dashboard').show();
+});
+
+
+
 // GET запрос - получить с сервера
 // POST запрос - отдать на сервер
 // необходим роут, который примет запрос. В контроллере для studio_members 
