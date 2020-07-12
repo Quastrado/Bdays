eeff27d93240f11105e750ba3042b38912ed346d
Quastrado <you@example.com>
2020-07-13 00:10:49 +0300

    #5 studio member DTO

---
+++ Bdays/DAL/DTO/studio_member.py
@@ -0,0 +1,9 @@
+
+
+class StudioMember():
+    def __init__(self, email, nickname, birthday, password, avatar):
+        self.email = email
+        self.nickname = nickname
+        self.birthday = birthday
+        self.password = password
+        self.avatar = avatar
